5261aee9c908128e1e633f50ac7e583fa05e029e
kira0x1 <kira.eva@protonmail.com>
2021-12-12 17:29:00 +0300

    wip

--- day10/index.js
+++ day10/index.js
@@ -1,13 +1,7 @@
 const input = require("./helper");
 
-// const line = "{([(<{}[<>[]}>";
-// const line = input[4];
-// console.log(line);
-// const line = `<{([([[(<>()){}]>(<<{{`;
-
 const openers = ["{", "<", "(", "["];
 const closers = ["}", ">", ")", "]"];
-const illegals = [];
 
 const pairMap = new Map();
 pairMap.set(openers[0], closers[0]);
@@ -15,63 +9,66 @@ pairMap.set(openers[1], closers[1]);
 pairMap.set(openers[2], closers[2]);
 pairMap.set(openers[3], closers[3]);
 
-const chunks = [];
-const corrupedLines = [];
+const illegals = [];
 
-function findChunk() {
-  for (const line of corrupedLines) {
-    let depth = 1;
-    for (let i = 0; i < line.length; i++) {
-      const c = line[i];
-
-      for (let x = i + 1; x < line.length; x++) {
-        switch (line[x]) {
-          case c:
-            depth++;
-            break;
-          case pairMap.get(c):
-            if (--depth === 0) {
-              return;
-            }
-            break;
-        }
-
-        // console.log(pairMap.get(c));
-        illegals.push(pairMap.get(c));
-        break;
-      }
-    }
-  }
-}
+const line = input[1];
+console.log(line.length);
+getChunk(line);
 
 for (const line of input) {
-  findCorrupted(line);
 }
 
-function findCorrupted(line) {
-  let depth = 1;
-  for (let i = 0; i < line.length; i++) {
+/**
+ *
+ * @param {string} line
+ */
+function getChunk(line) {
+  openersStack = [];
+
+  let firstOpener = undefined;
+
+  let openerIndex = 0;
+  let i = 0;
+  for (i = 0; i < line.length; i++) {
     const c = line[i];
+    openerIndex = i + 1;
     if (openers.includes(c)) {
-      for (let x = i + 1; x < line.length; x++) {
-        switch (line[x]) {
-          case c:
-            depth++;
-            break;
-          case pairMap.get(c):
-            if (--depth === 0) {
-              break;
-            }
-            break;
-        }
-      }
+      firstOpener = c;
+      openersStack.push(c);
     }
   }
 
-  if (depth > 0) corrupedLines.push(line);
-}
+  let depth = openersStack.length;
+  let target = pairMap.get(firstOpener);
+  let firstChunk = undefined;
+  let clos = undefined;
 
-findChunk();
+  for (i = 0; i < line.length; i++) {
+    const c = line[i];
+
+    if (c === firstOpener) {
+      depth++;
+    }
+
+    if (c === target) {
+      if (--depth === 0)
+        return getChunk(
+          (firstChunk = line.slice(openerIndex + 1, line.length))
+        );
+    }
+
+    if (depth === 1 && closers.includes(c)) {
+      clos = c;
+    }
+  }
+
+  if (clos === undefined) {
+    return getChunk(line.slice(openerIndex + 1, line.length));
+  } else {
+    illegals.push(clos);
+    console.log(`expecting ${target} but recieved ${clos}`);
+  }
+}
 
 let paran = 0;
 let bracket = 0;
@@ -98,34 +95,3 @@ const gtScore = gt * 25137;
 console.log(
   `final score: ${paranScore + bracketScore + squiggleScore + gtScore}`
 );
-
-function logChunks() {
-  for (const c of chunks) {
-    let r = "";
-    const op = c[0];
-    const target = pairMap.get(op);
-    for (const l of c) {
-      r += l;
-    }
-    console.log(r);
-  }
-}
-
-// console.table(openChunks);
-// console.table(closeChunks);
-
-function findOpeners(line) {
-  const op = [];
-  let i = 0;
-  for (const c of line) {
-    if (openers.includes(c)) op.push({ c, i });
-    i++;
-  }
-  return op;
-}
-
-function findClosers(c) {
-  const found = closers.find((o) => o === c);
-  closingChunk = found;
-  if (found) console.log(found);
-}
---
+++ day10/old.js
@@ -0,0 +1,133 @@
+const input = require("./helper");
+
+// const line = "{([(<{}[<>[]}>";
+// const line = input[4];
+// console.log(line);
+// const line = `<{([([[(<>()){}]>(<<{{`;
+
+const openers = ["{", "<", "(", "["];
+const closers = ["}", ">", ")", "]"];
+const illegals = [];
+
+const pairMap = new Map();
+pairMap.set(openers[0], closers[0]);
+pairMap.set(openers[1], closers[1]);
+pairMap.set(openers[2], closers[2]);
+pairMap.set(openers[3], closers[3]);
+
+const chunks = [];
+const corrupedLines = [];
+
+function findChunk() {
+  for (const line of corrupedLines) {
+    let depth = 1;
+    for (let i = 0; i < line.length; i++) {
+      const c = line[i];
+
+      for (let x = i + 1; x < line.length; x++) {
+        switch (line[x]) {
+          case c:
+            depth++;
+            break;
+          case pairMap.get(c):
+            if (--depth === 0) {
+              return;
+            }
+            break;
+        }
+
+        // console.log(pairMap.get(c));
+        illegals.push(pairMap.get(c));
+        break;
+      }
+    }
+  }
+}
+
+for (const line of input) {
+  findCorrupted(line);
+}
+
+function findCorrupted(line) {
+  let depth = 1;
+  for (let i = 0; i < line.length; i++) {
+    const c = line[i];
+    if (openers.includes(c)) {
+      for (let x = i + 1; x < line.length; x++) {
+        switch (line[x]) {
+          case c:
+            depth++;
+            break;
+          case pairMap.get(c):
+            depth -= 1;
+            break;
+        }
+      }
+    }
+  }
+
+  console.log(line);
+  console.log(depth);
+  console.log(`\n`);
+  if (depth > 1 && depth < 20) corrupedLines.push(line);
+}
+
+console.log(corrupedLines);
+findChunk();
+
+let paran = 0;
+let bracket = 0;
+let squigle = 0;
+let gt = 0;
+
+for (const i of illegals) {
+  if (i === ">") gt++;
+  if (i === ")") paran++;
+  if (i === "}") squigle++;
+  if (i === "]") bracket++;
+}
+
+console.log(`): ${paran}`);
+console.log(`]: ${bracket}`);
+console.log(`}: ${squigle}`);
+console.log(`>: ${gt}`);
+
+const paranScore = paran * 3;
+const bracketScore = bracket * 57;
+const squiggleScore = squigle * 1197;
+const gtScore = gt * 25137;
+
+console.log(
+  `final score: ${paranScore + bracketScore + squiggleScore + gtScore}`
+);
+
+function logChunks() {
+  for (const c of chunks) {
+    let r = "";
+    const op = c[0];
+    const target = pairMap.get(op);
+    for (const l of c) {
+      r += l;
+    }
+    console.log(r);
+  }
+}
+
+// console.table(openChunks);
+// console.table(closeChunks);
+
+function findOpeners(line) {
+  const op = [];
+  let i = 0;
+  for (const c of line) {
+    if (openers.includes(c)) op.push({ c, i });
+    i++;
+  }
+  return op;
+}
+
+function findClosers(c) {
+  const found = closers.find((o) => o === c);
+  closingChunk = found;
+  if (found) console.log(found);
+}
